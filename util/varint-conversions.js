e7f5de38ec62cfd30f6258862ca23faa754efbf3
Manu Sporny <msporny@digitalbazaar.com>
2026-04-11 12:00:01 -0400

    Add varint conversion test.

---
+++ util/varint-conversions.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright (c) 2023 Digital Bazaar, Inc. All rights reserved.
+ */
+import varint from 'varint';
+
+// converts varint to hex
+function vintToHex(data) {
+  if(typeof data !== 'number') {
+    throw new TypeError('"data" must be a number.');
+  }
+  return varint.encode(data);
+}
+
+// converts ECDSA key headers from varint to hex
+function main() {
+  console.log('varint -> hex:');
+  console.log(`0x1210 -> 0x${Buffer.from(vintToHex(0x1210)).toString('hex')}`);
+}
+
+main();
